a76db578820fe8cb9910a7895c5deb9b3e654bfd
juleskreutzer <j.kreutzer@student.fontys.nl>
2015-08-13 22:30:20 +0200

    Added nowPlaying for specific speaker

--- static/js/custom.js
+++ static/js/custom.js
@@ -47,47 +47,6 @@ function IncreaseVolumeAllSpeakers() {
 }
 // END Functions all Speakers
 
-function CreateDefaultSetting() {
-  // +_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+
-  // DO NOT CHANGE THESE SETTING WHEN YOU DON'T KNOW WHAT YOU ARE DOING !!
-  // +_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+
-
-  // Create the default settings for the web controller
-
-  // Settings for volumes
-  localStorage.setItem("Volume-off", "0");
-  localStorage.setItem("Volume-decrease", "-5");
-  localStorage.setItem("Volume-increase", "+5");
-  localStorage.setItem("Default-volume", "15");
-
-  // Setting for zones
-  $.ajax({
-    url: "/Zones"
-  }).done(function(data){
-    var ZonesArray = "["
-    jQuery.each(data, function(){
-      ZonesArray = ZonesArray + "\"" + this.coordinator.roomName + "\",";
-    })
-    ZonesArray = ZonesArray.replace(/,\s*$/, "");
-    ZonesArray = ZonesArray + "]";
-    localStorage.setItem("Zones", ZonesArray);
-  });
-}
-
-function FillSelectWithZones(){
-  var obj = JSON.parse(localStorage.getItem("Zones"));
-  var i = 0;
-  jQuery.each(obj, function(){
-    if(obj[i] == localStorage.getItem("SelectedZone")){
-      $('#SelectZone').append('<option value=' + obj[i] + ' selected>' + obj[i] + '</option>');
-    }
-    else {
-      $('#SelectZone').append('<option value=' + obj[i] + '>' + obj[i] + '</option>');
-    }
-    i = i + 1;
-  });
-}
-
 // Functions for Specific Speakers
 function PauseSpecificSpeaker(){
   jQuery.ajax({
@@ -116,5 +75,77 @@ function DecreaseVolumeSpecificSpeaker(){
 function MuteSpecificSpeaker(){
   jQuery.ajax({
     url: '/' + localStorage.getItem("SelectedZone") + '/volume/' + localStorage.getItem("Volume-off")
+  });
+}
+// END Functions for Specific Speakers
+
+// Other Functions
+function CreateDefaultSettings() {
+  // +_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+
+  // DO NOT CHANGE THESE SETTING WHEN YOU DON'T KNOW WHAT YOU ARE DOING !!
+  // +_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+
+
+  // Create the default settings for the web controller
+
+  // Settings for volumes
+  localStorage.setItem("Volume-off", "0");
+  localStorage.setItem("Volume-decrease", "-5");
+  localStorage.setItem("Volume-increase", "+5");
+  localStorage.setItem("Default-volume", "15");
+
+  // Setting for zones
+  $.ajax({
+    url: "/Zones"
+  }).done(function(data){
+    console.log(data);
+    var ZonesArray = "["
+    jQuery.each(data, function(){
+      ZonesArray = ZonesArray + "\"" + this.coordinator.roomName + "\",";
+    })
+    ZonesArray = ZonesArray.replace(/,\s*$/, "");
+    ZonesArray = ZonesArray + "]";
+    localStorage.setItem("Zones", ZonesArray);
+  });
+}
+
+function FillSelectWithZones(){
+  var obj = JSON.parse(localStorage.getItem("Zones"));
+  var i = 0;
+  jQuery.each(obj, function(){
+    if(obj[i] == localStorage.getItem("SelectedZone")){
+      $('#SelectZone').append('<option value=' + obj[i] + ' selected>' + obj[i] + '</option>');
+    }
+    else {
+      $('#SelectZone').append('<option value=' + obj[i] + '>' + obj[i] + '</option>');
+    }
+    i = i + 1;
+  });
+}
+
+function ShowCurrentPlaying(Zone){
+  var obj;
+  console.log(Zone);
+  jQuery.ajax({
+    url: '/Zones'
+  }).done(function(data){
+    jQuery.each(data, function(){
+      if(this.coordinator.roomName == Zone)
+      {
+        console.log(this);
+        $("#nowPlaying").empty();
+
+        if(this.coordinator.state.zoneState == "PLAYING"){
+        $("#nowPlaying").append('<img src="http://i169.photobucket.com/albums/u236/darkashe_4002/GIF/equalizerblue.gif"/>&nbsp;');
+        }
+
+        $("#nowPlaying").append(this.coordinator.state.currentTrack.title);
+
+        if(this.coordinator.state.currentTrack.type == "radio"){
+          var radioShowMetaData = this.coordinator.state.currentTrack.radioShowMetaData;
+          radioShowMetaData = radioShowMetaData.substring(0, radioShowMetaData.indexOf(","));
+          $("#nowPlaying").append(" - " + radioShowMetaData);
+        }
+      }
+    })
   });
 }
