8a0f2ea32fe36d40f48204f411b14ae2874ba631
Jules Kreutzer <juleskreutzer@me.com>
2015-12-10 21:36:29 +0100

    want github

--- server.js
+++ server.js
@@ -53,6 +53,49 @@ var server = http.createServer(function (req, res) {
     });
   }).resume();
 });
+var bridgeDiscovery = require("./hue-api/bridge-discovery")
+    , Hue = require("./hue-api")
+    , lightState = require("./hue-api/lightstate")
+    , scheduledEvent = require("./hue-api/scheduledEvent.js")
+    , ApiError = require("./hue-api/errors.js").ApiError
+    ;
+
+
+function deprecated(fn, message) {
+    return function () {
+        var args = Array.prototype.slice.call(arguments);
+        console.error(message);
+        return fn.apply(this, args);
+    };
+}
+
+
+module.exports = {
+    HueApi: Hue,
+    BridgeApi: Hue,
+    api: Hue,
+
+    //TODO document this
+    connect: function(config) {
+        return new Hue(config);
+    },
+
+    lightState: lightState,
+    scheduledEvent: scheduledEvent,
+
+    upnpSearch: bridgeDiscovery.networkSearch,
+    nupnpSearch: bridgeDiscovery.locateBridges,
+
+    locateBridges: deprecated(bridgeDiscovery.locateBridges
+        , "'locateBridges' is deprecated, please use 'nupnpSearch' instead"),
+
+    searchForBridges: deprecated(
+        bridgeDiscovery.networkSearch
+        , "'searchForBridges' is deprecated, please use 'upnpSearch' instead"
+    ),
+
+    ApiError: ApiError
+};
 
 server.listen(settings.port, function () {
   console.log('http server listening on port', settings.port);
